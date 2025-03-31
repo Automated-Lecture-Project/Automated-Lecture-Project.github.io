ff22cab31de8ec68c26a954a2a08409380833be7
sjwetzel <sebastian.wetzel@gmx.net>
2025-03-31 19:12:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function sendMail(){
     let params ={
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
-        message : document.getElementById("message").value
+        message : document.getElementById("message").value,
     }
 
-    emailjs.send("service_qm1cdrl","template_isy0jjo",params).then(alert("Email Sent."))
+    emailjs.send("service_qm1cdrl","template_isy0jjo",params).then(alert("Email Sent."));
 }
