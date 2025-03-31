f5fa2c02a398c62192d68df7471fec8a014ed519
sjwetzel <sebastian.wetzel@gmx.net>
2025-03-31 19:08:11 +0200

    komma

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function sendMail(){
     let params ={
-        name : document.getElementById("name").value
-        email : document.getElementById("email").value
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
         message : document.getElementById("message").value
     }
 
